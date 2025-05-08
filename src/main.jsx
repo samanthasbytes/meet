e400d6ac6a4a9995aab5d9f4e7aaaac36c60df36
Samantha <158760603+samanthasbytes@users.noreply.github.com>
2025-05-08 19:57:24 -0400

    Task 4.7: add Atatus for error tracking

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import * as atatus from 'atatus-spa';
+
+atatus.config('daff2cc8d313467496d2faa721e073c9').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
