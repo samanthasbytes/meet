11c805bc7ebee4e484685990852a07ad79dcf91b
Samantha <158760603+samanthasbytes@users.noreply.github.com>
2025-10-21 11:51:07 -0400

    Refactor CityEventsChart: improve XAxis

--- src/components/CityEventsChart.jsx
+++ src/components/CityEventsChart.jsx
@@ -12,7 +12,7 @@ const CityEventsChart = ({ allLocations, events }) => {
   const getData = () => {
     const data = allLocations.map((location) => {
       const count = events.filter((event) => event.location === location).length;
-      const city = location.split(', ')[0];
+      const city = location.split(/, | - /)[0];
       return { city, count };
     });
     return data;
@@ -29,7 +29,14 @@ const CityEventsChart = ({ allLocations, events }) => {
         }}
       >
         <CartesianGrid />
-        <XAxis type="category" dataKey="city" name="City" />
+        <XAxis
+          type="category"
+          dataKey="city"
+          name="City"
+          angle={60}
+          interval={0}
+          tick={{ dx: 20, dy: 40, fontSize: 14 }}
+        />
         <YAxis type="number" dataKey="count" name="Number of events" allowDecimals={false} />
         <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Scatter name="A school" data={data} fill="#8884d8" />
